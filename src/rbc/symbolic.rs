ef67c99415a102d3bb06131e3bbcfc023453a68d
Stefano <stefano.varotti@unitn.it>
2017-11-22 14:55:47 +0100

    truth table eval

--- src/rbc/symbolic.rs
+++ src/rbc/symbolic.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 use std::vec;
 use super::aig;
 
+extern crate generic_array;
+use self::generic_array::*;
+
 pub trait BooleanLike {
     fn truth() -> Self;
     fn conjunction(&Self, &Self) -> Self;
@@ -12,6 +15,37 @@ pub trait SymbolicEval<K, B: BooleanLike + Clone> {
     fn eval_symbolic(&self, &mut HashMap<K, B>) -> B;
 }
 
+impl<N:ArrayLength<bool>> BooleanLike for GenericArray<bool,N> {
+    fn truth() -> Self {
+        return Self::generate(|_| true);
+        }
+    fn conjunction(a: &Self, b: &Self) -> Self {
+        let mut ret = Self::default();
+        for (i,(&ai,&bi)) in a.iter().zip(b.iter()).enumerate(){
+            ret[i] = ai && bi;
+        }
+        return ret;
+    }
+    fn negation(a:&Self) -> Self {
+        let mut ret = Self::default();
+        for (i,&ai) in a.iter().enumerate(){
+            ret[i] = !ai;
+        }
+        return ret;
+    }
+} 
+
+impl BooleanLike for aig::AigEdge {
+    fn truth() -> Self{
+        aig::truen()
+    }
+    fn conjunction(a:&Self, b:&Self) -> Self {
+        aig::and(a,b)
+    }
+    fn negation(a:&Self) -> Self {
+        aig::not(a)
+    }
+}
 
 impl<B : BooleanLike + Clone > SymbolicEval<String, B> for aig::AigEdge {
     fn eval_symbolic(&self, a : &mut HashMap<String, B> ) -> B {
